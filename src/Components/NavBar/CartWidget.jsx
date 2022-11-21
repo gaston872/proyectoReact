4443d5671bd7ccce7ea41688552b201f2ba542c8
Gastón Aranda <gaston87_2@hotmail.com>
2022-11-20 21:18:23 -0300

    agrego navbar

---
+++ src/Components/NavBar/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CartWidget() {
+    return (
+        <img className="carrito" src="/img/shoppingcart2.png"></img>  
+    )
+}
+
+export default CartWidget
---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CartWidget from './CartWidget';
+import "./navbar.css";
+
+const stylesIcon = {
+    marginLeft: "30px"
+};
+
+function Navbar() {
+    return (
+        <div className="nav">
+            <img className="logo" src="/img/logo.png" alt="logo"></img>
+            <div className="navLink">
+            <a  href="#">Oleo</a>
+            <a  href="#">lapiz</a>
+            <a  href="#">Acuarela</a>
+            </div>
+            <CartWidget/>
+        </div>
+    )
+}
+
+export default Navbar
