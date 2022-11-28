1722dcee1421b997854157e00242ad56df1a217c
Gastón Aranda <gaston87_2@hotmail.com>
2022-11-27 22:29:26 -0300

    agrego card

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CartWidget from '../CartWidget/CartWidget';
+import "./itemListContainer.css";
 
 function ItemListContainer(props) {
+
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    function handleFavorite() {
+        setIsFavorite(!isFavorite);
+        /* isFavorite === true ? setIsFavorite(false) : setIsFavorite(true); */
+    }
+
     return (
-        <h1>{props.greeting}</h1>
+        <>
+            <h1>{props.greeting}</h1>
+            <div className="card">
+                <button
+                    onClick={handleFavorite}
+                    className={isFavorite ? "card-favicon favorite" : "card-favicon"}>
+                    ♥
+                </button>
+                <div className="cardImg">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCmVc5vdvACYKZnagLaFSFwrMASlPSh153Sg&usqp=CAU"
+                        alt="imagen" />
+                </div>
+                <h2 className="titulo">Titulo</h2>
+                <hr className="linea" />
+                <p className="info">info</p>
+                <div className="contador">
+                    <button>-</button>
+                    <span>1</span>
+                    <button>+</button>
+                </div>
+                <button className="boton-agregar">Agregar al carrito</button>
+            </div>
+        </>
     )
 }
 
