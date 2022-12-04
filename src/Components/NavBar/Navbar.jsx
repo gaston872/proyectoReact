ed3efc628caa419d8f3a00f85176b35e0f53477a
Gastón Aranda <gaston87_2@hotmail.com>
2022-12-04 19:05:20 -0300

    arreglo navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -13,10 +13,10 @@ function Navbar() {
             <img className="logo" src="/img/logo.png" alt="logo"></img>
             <div className="navLink">
                 <Link to="/">Inicio</Link>
-                <Link to="/item">Item</Link>
                 <Link to="/categoria/Oleo">Oleo</Link>
                 <Link to="/categoria/Acrílico">Acrílico</Link>
                 <Link to="/categoria/Acuarela">Acuarela</Link>
+                {/* <Link to="/item"></Link> */}
             </div>
             <CartWidget />
         </div>
